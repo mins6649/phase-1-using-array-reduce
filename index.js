7fc957e28e5c436af554a94352aad9a539520dac
minyoungshin <mins6649@gmail.com>
2023-02-06 10:37:26 -0500

    array.reduce

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 const sumBatteries = function(){
     const initalValue = 0;
+    //you need to add the initial value after: (accumulator + currentValue, initalValue) to tell the function where we start adding.
    return batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, initalValue);
 
 }
